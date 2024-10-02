9c33df7068a0a411e19509bb55fdf5d420ead8f5
nitnelav55 <55val55@gmail.com>
2024-10-02 16:13:30 +0200

    SECOND CHANGE

---
+++ src/Entity/Book.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Entity;
+
+use App\Enum\BookStatus;
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $cover = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $editedAt = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?text $plot = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $pageNumber = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?BookStatus $status = null;
+
+    #[ORM\ManyToOne(inversedBy: 'books')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Editor $editor = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(?string $isbn): static
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getCover(): ?string
+    {
+        return $this->cover;
+    }
+
+    public function setCover(?string $cover): static
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    public function getEditedAt(): ?\DateTimeImmutable
+    {
+        return $this->editedAt;
+    }
+
+    public function setEditedAt(\DateTimeImmutable $editedAt): static
+    {
+        $this->editedAt = $editedAt;
+
+        return $this;
+    }
+
+    public function getPlot(): ?text
+    {
+        return $this->plot;
+    }
+
+    public function setPlot(?text $plot): static
+    {
+        $this->plot = $plot;
+
+        return $this;
+    }
+
+    public function getPageNumber(): ?int
+    {
+        return $this->pageNumber;
+    }
+
+    public function setPageNumber(?int $pageNumber): static
+    {
+        $this->pageNumber = $pageNumber;
+
+        return $this;
+    }
+
+    public function getStatus(): ?BookStatus
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?BookStatus $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getEditor(): ?Editor
+    {
+        return $this->editor;
+    }
+
+    public function setEditor(?Editor $editor): static
+    {
+        $this->editor = $editor;
+
+        return $this;
+    }
+}
